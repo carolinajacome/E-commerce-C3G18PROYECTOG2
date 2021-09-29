7c50f9862ac26dbd48576c3f946d8358d5ff3be1
MarianaOrrego <mariana.orregof@gmail.com>
2021-09-29 18:19:03 -0500

    Formspark funcional

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -18,8 +18,17 @@
               </span>
             </v-col>
             <v-col :key="n" cols="6" sm="6">
-              <v-form ref="form" v-model="valid" lazy-validation>
+              <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+                action="https://submit-form.com/tMnpyl4y"
+              >
                 <v-text-field
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
                   v-model="name"
                   :counter="10"
                   :rules="nameRules"
@@ -27,25 +36,32 @@
                   required
                 ></v-text-field>
                 <v-text-field
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
                   v-model="email"
                   :rules="emailRules"
                   label="E-mail"
                   required
                 ></v-text-field>
                 <v-textarea
+                  id="message"
+                  name="message"
+                  placeholder="Message"
                   v-model="message"
                   :rules="messageRules"
                   label="Mensaje"
                   required
                 ></v-textarea>
                 <v-btn
+                  type="submit"
                   :disabled="!valid"
                   color="primary"
                   class="mr-4"
                   @click="validate"
                   >Enviar</v-btn
                 >
-                <v-btn color="error" class="mr-4" @click="reset">Limpiar</v-btn>
               </v-form>
             </v-col>
           </v-row>
@@ -56,7 +72,7 @@
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.110857739074!2d-72.50782048476677!3d7.883468908045121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e664543cde0afc1%3A0x3a0c02cdac172ce4!2sCentro%20Comercial%20La%20Estrella!5e0!3m2!1ses!2sco!4v1632234424375!5m2!1ses!2sco"
           width="100%"
           height="450"
-          style="border:0;"
+          style="border: 0"
           allowfullscreen=""
           loading="lazy"
         ></iframe>
@@ -66,25 +82,56 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import { useFormspark } from "@formspark/use-formspark";
+export default {
+  setup() {
+    const message = ref("");
+
+    const [submit, submitting] = useFormspark({
+      formId: "your-form-id",
+    });
+
+    const onInput = (e) => {
+      message.value = e.target.value;
+    };
+
+    const onSubmit = async (e) => {
+      e.preventDefault();
+      await submit({ message: message.value });
+      message.value = "";
+    };
+
+    return {
+      message,
+      onInput,
+      onSubmit,
+      submitting,
+    };
+  },
+};
+</script>
+
+<script>
 export default {
   name: "Contact",
   data: () => ({
     valid: true,
     name: "",
     nameRules: [
-      v => !!v || "Name is required",
-      v => (v && v.length <= 10) || "Name must be less than 10 characters"
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
     ],
     email: "",
     emailRules: [
-      v => !!v || "E-mail is required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
     ],
     message: "",
     messageRules: [
-      v => !!v || "Message is required",
-      v => (v && v.length >= 10) || "Message must be more than 10 characters"
-    ]
+      (v) => !!v || "Message is required",
+      (v) => (v && v.length >= 10) || "Message must be more than 10 characters",
+    ],
   }),
   methods: {
     validate() {
@@ -94,8 +141,8 @@ export default {
     },
     reset() {
       this.$refs.form.reset();
-    }
-  }
+    },
+  },
 };
 </script>
 
