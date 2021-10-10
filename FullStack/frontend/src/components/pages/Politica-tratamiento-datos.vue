2c4812a91d970331075227f8cc0cb7526dcf5b93
Mariana <mariana.orregof@gmail.com>
2021-10-09 19:11:50 -0500

    agg frontend realizado

---
+++ FullStack/frontend/src/components/pages/Politica-tratamiento-datos.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <v-carousel hide-delimiters>
+      <v-carousel-item
+        :src="require('../../assets/images/home/carousel/slider-4.jpg')"
+      >
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-2 white--text pl-5 pr-5 hidden-sm-only">
+            <strong>J FOUR JEANS</strong>
+          </div>
+          <br />
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+    <v-container>
+      <p class="display-3 font-weight-light	text-center pa-4">
+        Política de Tratamiento de datos personales
+      </p>
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-1">
+          <v-card class="mx-auto" max-width="1200">
+            <v-card-text class="text--primary">
+              <div>
+                Los datos personales suministrados por los Titulares a
+                jfourjeans.com serán usados exclusivamente para fines propios
+                del desarrollo de su objeto social y de una prestación efectiva
+                de los servicios o productos contratados, adquiridos o cotizados
+                relacionados con la venta de prendas de vestir, ya sea
+                directamente o a través de terceros, para fines publicitarios,
+                de promoción o comercialización de nuestros servicios, para
+                estudios o investigaciones de mercados, para informes o
+                contestaciones a la autoridades administrativas, judiciales o de
+                control, y para la cotización, adquisición, pago, reclamos,
+                solicitudes, garantías y devoluciones relacionadas con sus
+                proveedores. Los datos o información personal podrán ser
+                utilizados entre otros para los siguientes procesos: Reservas,
+                cancelaciones, modificaciones, reconfirmaciones, cotizaciones,
+                aviso de cambios, atención de quejas, comentarios y sugerencias,
+                programas de fidelización, verificación de tarjetas de crédito,
+                identificación de fraudes, prevención de lavado de activos,
+                comercio, promoción y gestión electrónica, para compartir datos
+                con terceros cuyos servicios sean similares y con fines
+                relacionados con la comercialización, publicidad y promoción de
+                nuestros productos. Igualmente, para suministrar datos a las
+                autoridades judiciales, de control, administrativas o
+                gubernamentales y para la defensa y protección de los derechos
+                de jeansymoda.com para permitir el acceso a terceros con quienes
+                se contraten los servicios de almacenamiento y procesamiento de
+                la información, software, y demás servicios electrónicos.
+                <v-list>VIGENCIA DEL TRATAMIENTO DE DATOS PERSONALES</v-list>
+                La información suministrada por los pasajeros, usuarios,
+                proveedores, o clientes podrá ser almacenada de manera
+                indefinida cuando se considere necesario.
+
+                <v-list>
+                  CANALES DE ATENCIÓN PARA EL TRATAMIENTO DE DATOS
+                  PERSONALES</v-list
+                >
+                El área de Servicio al Cliente está encargada de atender las
+                inquietudes, reclamos, trámites de supresión, revocatoria,
+                aclaraciones, actualizaciones, rectificaciones y demás
+                solicitudes relacionadas con el tratamiento de datos personales.
+                Puede comunicarse a la línea en Colombia (57) 3223658208 o al
+                correo electrónico: infojfourjeans@gmail.com
+              </div>
+            </v-card-text>
+          </v-card>
+        </div>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 4,
+    };
+  },
+};
+</script>
